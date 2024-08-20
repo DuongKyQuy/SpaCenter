110d18250cfce8f9c667d537265e8d71d539f97f
Quy Duong <duongkyquy18@gmail.com>
2024-08-20 14:50:17 +0700

    feat: fetch api forgot

--- Frontend/pages/forgotPassword/forgotPassword.js
+++ Frontend/pages/forgotPassword/forgotPassword.js
@@ -1,18 +1,3 @@
-document.getElementById("forgotForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  var email = document.getElementById("email").value.trim();
-
-  var valid = true;
-
-  if (email === "") {
-    document.getElementById("emailError").style.display = "inline";
-    document.getElementById("emailError").style.fontSize = "12px";
-    valid = false;
-  } else {
-    document.getElementById("emailError").style.display = "none";
-  }
-});
-
 const form = document.getElementById("forgotForm");
 
 form.addEventListener("submit", function (event) {
